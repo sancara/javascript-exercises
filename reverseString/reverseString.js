e0802e5987b76d7cd0364e9a2fb9443a814dd50e
Sancara <santiagocaraballo@gmail.com>
2021-05-29 15:10:28 -0300

    reverseString done!

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let palabra = ""
+    for (let i = word.length-1; i >= 0; i--) {
+        palabra += word[i]   
+    }
+    return palabra;
 };
 
 module.exports = reverseString;
