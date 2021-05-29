7e60b2493bd69d3c5c4a7dd93d2c8b9c91cc0add
Sancara <santiagocaraballo@gmail.com>
2021-05-29 11:50:34 -0300

    repeatString done!

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    let result = ""
+    if (number > 0) {
+        for (let i = 0; i < number; i++) {
+            result += word 
+        }
+        } else if (number < 0) {
+            return 'ERROR';
+        }    
+    return result;
+        
 };
 
 module.exports = repeatString;
